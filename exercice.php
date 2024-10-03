13453786a65911f97776fc81cd04119ec9ba50ce
HelenemLH <helenemlagarde@gmail.com>
2024-10-03 15:02:26 +0200

    typo

--- exercice.php
+++ exercice.php
@@ -5,12 +5,12 @@ session_start();
 // vérif via GET 
 if (isset($_GET['first_name']) && !empty($_GET['first_name'])) {
     $firstName = htmlspecialchars($_GET['first_name']);
-    $_SESSION['first_name'] = $firstName;  // Mettre à jour la session avec ce prénom
+    $_SESSION['first_name'] = $firstName;  // mettre à jour session avec ce prénom
 }
 // sinon verif via POST
 elseif (isset($_POST['first_name']) && !empty($_POST['first_name'])) {
     $firstName = htmlspecialchars($_POST['first_name']);
-    $_SESSION['first_name'] = $firstName;  // Sauvegarder dans la session
+    $_SESSION['first_name'] = $firstName;  // save session
 }
 // sinon utiliser session
 elseif (isset($_SESSION['first_name'])) {
@@ -32,3 +32,4 @@ echo "Bonjour " . $firstName;
 
 <!-- reset button -->
 <p><a href="exercice.php?reset_session=true">Reset</a></p>
+
